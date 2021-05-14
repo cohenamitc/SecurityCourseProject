d8787eb1740db9c34d39a66910dce346d7ed23f8
Amit Cohen <amitcohen@microsoft.com>
2021-05-14 14:55:26 +0300

    seclib

---
+++ app/sec_lib/sec_lib.py
@@ -0,0 +1,10 @@
+class SecLib(object):
+    @staticmethod
+    def prevent_xss_encoding(to_encode: str) -> str:
+        # TODO: Implement prevent_xss_encoding
+        raise NotImplementedError
+
+    @staticmethod
+    def prevent_sqli_encoding(to_encode: str) -> str:
+        # TODO: Implement prevent_sqli_encoding
+        raise NotImplementedError
